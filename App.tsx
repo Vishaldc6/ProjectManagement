f38761ed61ac7aafbc45ef1627501d91bda0ab44
Viresh Kumar Mistry <vireshkumarmistry@gmail.com>
2026-01-09 11:56:47 +0530

    - Setup app logo and splash screen

--- App.tsx
+++ App.tsx
@@ -6,14 +6,24 @@
  */
 
 import { NewAppScreen } from '@react-native/new-app-screen';
+import { useEffect } from 'react';
 import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
+import BootSplash from 'react-native-bootsplash';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
+
+  useEffect(() => {
+    hideSplash();
+  }, []);
+
+  async function hideSplash() {
+    await BootSplash.hide({ fade: true });
+  }
 
   return (
     <SafeAreaProvider>
